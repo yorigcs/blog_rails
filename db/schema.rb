5cf4d4ff3b9f835773a6804e2adaf097b3a883e6
Yorí Galisteu <yori.silva@investtools.com.br>
2024-02-26 19:25:05 -0300

    add comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_26_013520) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_26_214820) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "public"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status", default: "public"
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  add_foreign_key "comments", "articles"
 end
